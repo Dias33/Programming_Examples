945e2927948498bdad3c888bd48dcec5b9cf7dc1
Andrei Niculae <andrei.niculae96@gmail.com>
2018-05-17 14:18:15 +0100

    Add files via upload

---
+++ AI_Pathfinding/Node.cs
@@ -0,0 +1,62 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Each node has a g-cost and an h-cost.
+ * G-cost is a value that represents the distance from the starting node.
+ * H-cost is a value that represents the distance from the end node.
+ * F-cost = G-cost + H-cost
+ */
+
+public class Node : IHeapItem<Node>
+{
+	public bool walkable;
+	public Vector3 world_position;
+	public int grid_x;
+	public int grid_y;
+
+	public int g_cost;
+	public int h_cost;
+	public Node parent;
+    int heapIndex;
+
+	public Node (bool walkable_, Vector3 world_position_, int grid_x_, int grid_y_)
+	{
+		walkable = walkable_;
+		world_position = world_position_;
+		grid_x = grid_x_;
+		grid_y = grid_y_;
+	}
+
+	public int f_Cost()
+	{
+		return g_cost + h_cost;
+	}
+
+    /*
+    * Creates a reference Heap index of this node.
+    * This allows each node to remember their index in the Heap data structure.
+    */
+
+    public int HeapIndex
+    {
+        get { return heapIndex; }
+        set { heapIndex = value; }
+    }
+
+    /*
+    * Compares two nodes based on their f-costs.
+    */
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = f_Cost().CompareTo(nodeToCompare.f_Cost());
+
+        if (compare == 0)
+        {
+            compare = h_cost.CompareTo(nodeToCompare.h_cost);
+        }
+
+        return -compare;
+    }
+}
---
+++ AI_Pathfinding/PathRequestManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+/*
+ * In order to allow the pathfinding algorithm to calculate multiple paths at the same time,
+ * a manager of path requests has been created.
+ * This is a queue that store each request and then tries to processes them one by one as fast as possible.
+ */ 
+
+public class PathRequestManager : MonoBehaviour {
+
+    Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
+    PathRequest currentPathRequest;
+
+    static PathRequestManager instance;
+    Pathfinding pathfinding;
+
+    bool isProcessingPath;
+
+    void Awake()
+    {
+        pathfinding = GetComponent<Pathfinding>();
+        instance = this;
+    }
+
+    public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback)
+    {
+        PathRequest newRequest = new PathRequest (pathStart, pathEnd, callback);
+        instance.pathRequestQueue.Enqueue(newRequest);
+        instance.TryProcessNext();
+    }
+
+    void TryProcessNext()
+    {
+        if (!isProcessingPath && pathRequestQueue.Count > 0)
+        {
+            currentPathRequest = pathRequestQueue.Dequeue();
+            isProcessingPath = true;
+            pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+        }
+    }
+
+    public void FinishedProcessingPath(Vector3[] path, bool success)
+    {
+        currentPathRequest.callback(path, success);
+        isProcessingPath = false;
+        TryProcessNext();
+    }
+
+    struct PathRequest
+    {
+        public Vector3 pathStart;
+        public Vector3 pathEnd;
+        public Action<Vector3[], bool> callback;
+
+        public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback)
+        {
+            pathStart = _start;
+            pathEnd = _end;
+            callback = _callback;
+        }
+    }
+}
